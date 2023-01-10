493686e532857d3d1bfe13f18f6f6e41a1c09dbe
Kamil <herbetko.kamil@gmail.com>
2023-01-10 20:26:09 +0100

    Reshaping project for our needs and creating a database with models.

--- website/__init__.py
+++ website/__init__.py
@@ -10,16 +10,13 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site4.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+
+from website.models import *
 db.create_all()
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'  #przechodzimy do strony logowania jeśli nie mamy do czegoś uprawnień
-login_manager.login_message_category = 'info'   #ustawiamy styl tej wiadomości z bootstrapa
 
-# from website.models import *
-# # admin1 = Klient(imie='Admin', login='admin11@admin.com', haslo='12345678', jestAdminem=True)
-# # db.session.add(admin1)
-# # db.session.commit()
-#
+
+
 from website import routes
 
--- website/models.py
+++ website/models.py
@@ -1,47 +1,36 @@
 from datetime import datetime
-from website import db, login_manager
-from flask_login import UserMixin
+from website import db
 from abc import ABCMeta, abstractmethod
-from enum import Enum
-# import datetime
-
-class StatusPostu(Enum):
-    nowy = 'Nowy'
-    edytowany = 'Edytowany'
-    usuniety = 'Usunięty'
+from enum import Enum, unique
 
 
-class Klient(db.Model, UserMixin):
-    # __tablename__ = "klient"
+class Status(Enum):
+    dostarczona = "Dostarczona"
+    nadana = "Nadana"
+
+
+class Kurier(db.Model):
+    __tablename__ = "Kurier"
+
     id = db.Column(db.Integer, primary_key=True)
-    imie = db.Column(db.String(20), unique=True, nullable=True)
-    login = db.Column(db.String(100), unique=True, nullable=False) #login to email
-    image_file = db.Column(db.String(20), nullable=True, default='default.jpg')
-    haslo = db.Column(db.String(60), nullable=False)
-    jestAdminem = db.Column(db.Boolean, nullable=True, default=False)
-    dataUrodzenia = db.Column(db.Date, nullable=True)
-    posts = db.relationship('Post', backref='author', lazy=True)    #relacja
+    nazwa = db.Column(db.String(30), unique=True, nullable=False)
 
-    #rachunki = db.relationship('Rachunek', backref='author', lazy=True)
-    #transakcje
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return Klient.query.get(int(user_id))  # był tu user
-    def __repr__(self):
-        return f"Użytkownik {self.imie}, {self.login}, {self.image_file}, {self.haslo}"
+class Paczka(db.Model):
+    __tablename__ = "Paczka"
 
-class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tytul = db.Column(db.String(150), nullable=False)
-    dataUtworzenia = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-    # status = db.Column(db.String(20), nullable=False, default='Nowy')
-    tresc = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("klient.id"), nullable=False) # user.id z małej bo odwołujemy się do tablicy
-
-    def __repr__(self):
-        return f"Post {self.tytul}, {self.dataUtworzenia}"
+    nazwa = db.Column(db.String(30), unique=True, nullable=False)
 
 
-
+class Dostarczenia(db.Model):
+    __tablename__ = "Dostarczenia"
+    kurier_id = db.Column(
+        db.Integer, db.ForeignKey("Kurier.id"), primary_key=True, nullable=False
+    )
+    paczka_id = db.Column(
+        db.Integer, db.ForeignKey("Paczka.id"), primary_key=True, nullable=False
+    )
+    dataUtworzenia = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    status = db.Column(db.Enum(Status), nullable=False)
 
